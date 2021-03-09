af5379a12665487ebb7a8da8a6ef0a5d4107172c
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2021-03-09 05:38:57 -0500

    Added MongoDB connection

---
+++ backend/routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// @route     POST api/users
+// @desc      Register a user
+router.post('/', (req, res) => {
+    res.send('Register a user');
+});
+
+module.exports = router;
