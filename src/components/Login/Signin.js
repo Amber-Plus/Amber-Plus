45a97ff65a59c6560cb56be653407631a8bb5a5a
Raju Dahal <raju@MacBook-Pro.verizon.net>
2021-03-11 19:44:54 -0500

    added login to homepage

---
+++ src/components/Login/Signin.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Field from './field/field';
+
+const Signin = ({action}) => {
+  let id = 0;
+  id++;
+  return (
+    <form onSubmit={action}>
+      <div className='form-group'>
+        <label htmlFor='username'>Username</label>
+        <Field
+          id={'SignUp' + id++}
+          name='username'
+          type='text'
+          placeholder='ex. john.smith'
+        />
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='username'>Password</label>
+        <Field
+          id={'SignUp' + id++}
+          name='password'
+          type='password'
+          placeholder='******'
+        />
+      </div>
+
+      <button
+        id={'SignUp' + id++}
+        name='submit'
+        type='submit'
+        className='btn btn-success'
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+export default Signin;
