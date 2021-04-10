e1f922104afd996c6583035b045c1abd5b33b7a2
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2021-04-10 15:02:07 -0400

    Added Auth Context for login. Login form needs revision.

--- src/components/Login/Signin.js
+++ src/components/Login/Signin.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState, useContext, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Grid,
   Paper,
@@ -8,19 +8,20 @@ import {
   Button,
   Typography,
   Link,
-} from "@material-ui/core";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import { useSetRecoilState } from "recoil";
-import { loginStatus, userStatus } from "state/loginState";
-import { testUserData } from "constants/testUserData";
-import handleNavigation from "utils/handleNavigation";
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { useSetRecoilState } from 'recoil';
+import { loginStatus, userStatus } from 'state/loginState';
+import { testUserData } from 'constants/testUserData';
+// import handleNavigation from 'utils/handleNavigatsion';
+import AuthContext from '../../context/auth/authContext';
 
 const useStyles = makeStyles((theme) => ({
   paperStyle: {
     padding: 20,
-    height: "70vh",
+    height: '70vh',
     width: 280,
-    margin: "20px auto",
+    margin: '20px auto',
   },
   avatarStyle: {
     backgroundColor: theme.palette.secondary.light,
@@ -29,63 +30,82 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 1),
   },
   title: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginTop: theme.spacing(1.5),
   },
 }));
 
-const Signin = () => {
+const Signin = (props) => {
+  const authContext = useContext(AuthContext);
+
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+  }, [error, isAuthenticated, props.history]);
+
   const classes = useStyles();
   const [username, setUsername] = useState();
   const [pass, setPass] = useState();
-  const [user, setUser] = useState({ name: "", id: "" });
+  const [user, setUser] = useState({ email: '', password: '' });
   const setLoginState = useSetRecoilState(loginStatus);
   const setUserState = useSetRecoilState(userStatus);
 
+  const { email, password } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
   const handleSubmit = () => {
-    const profile = testUserData.find(
-      (person) => person.email === username && person.pass === pass
-    );
-    setUser(profile);
-    setLoginState("logout");
-    setUserState({ username: username, password: pass });
+    if (email === '' || password === '') {
+      login({
+        email,
+        password,
+      });
+    }
   };
 
   return (
     <Grid>
       <Paper elevation={10} className={classes.paperStyle}>
-        <Grid align="center">
+        <Grid align='center'>
           <Avatar className={classes.avatarStyle}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography variant="h3" className={classes.title}>
+          <Typography variant='h3' className={classes.title}>
             Login
           </Typography>
         </Grid>
         <TextField
-          id="username"
-          label="Username"
-          placeholder="Enter username"
-          onChange={(e) => setUsername(e.target.value)}
+          type='email'
+          name='email'
+          value={email}
+          id='email'
+          label='Email'
+          placeholder='Enter username'
+          onChange={onChange}
           fullWidth
           required
         />
         <TextField
-          id="password"
-          label="Password"
-          placeholder="Enter password"
-          type="password"
-          onChange={(e) => setPass(e.target.value)}
+          name='password'
+          value={password}
+          id='password'
+          label='Password'
+          placeholder='Enter password'
+          type='password'
+          onChange={onChange}
           fullWidth
           required
         />
         <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          component="a"
+          type='submit'
+          color='primary'
+          variant='contained'
+          component='a'
           onClick={() => handleSubmit()}
-          href={handleNavigation("profile", user.name, user.id)}
+          // href={handleNavigation('profile', user.name, user.id)}
           className={classes.btnStyle}
           fullWidth
         >
@@ -93,8 +113,8 @@ const Signin = () => {
         </Button>
 
         <Typography>
-          {" "}
-          Don't have an account? <Link href="/login">Sign Up</Link>
+          {' '}
+          Don't have an account? <Link href='/login'>Sign Up</Link>
         </Typography>
       </Paper>
     </Grid>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { RecoilRoot } from "recoil";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import Routes from "./routes";
-import theme from "./theme";
-import "leaflet/dist/leaflet.css";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import Routes from './routes';
+import theme from './theme';
+import 'leaflet/dist/leaflet.css';
+import './index.css';
+import AuthState from './context/auth/AuthState';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <MuiThemeProvider theme={theme}>
-        <Routes />
-      </MuiThemeProvider>
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+  <AuthState>
+    <React.StrictMode>
+      <RecoilRoot>
+        <MuiThemeProvider theme={theme}>
+          <Routes />
+        </MuiThemeProvider>
+      </RecoilRoot>
+    </React.StrictMode>
+  </AuthState>,
+  document.getElementById('root')
 );
