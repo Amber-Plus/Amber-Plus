666b83d3afb5152e1e8fba8c90d23f4187d26614
MichelleLucero <Michelle.Lucero07@myhunter.cuny.edu>
2021-04-10 02:55:12 -0400

    Users can be registered and login through the backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -8,49 +11,69 @@ const User = require('../models/User');
 // @desc    Register user
 // @access  Public
 router.post(
-    '/',
-    [
-        check('name', 'Name is required')
-            .not()
-            .isEmpty(),
-        check('email', 'Please include a valid email')
-            .isEmail(),
-        check('password', 'Enter a password with 6 or more charaters')
-            .isLength({ min: 6 })
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        // Destructuring
-        const { name, email, password } = req.body;
-
-        try {
-            let user = await User.findOne({ email: req.body.email });
-
-            if (user) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'User already exists' }] });
-            }
-
-            const avatar = "#";
-
-            user = new User({
-                name,
-                email,
-                avatar,
-                password
-            });
-
-            await user.save();
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
+  '/',
+  [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Please include a valid email').isEmail(),
+    body('password', 'Enter a password with 6 or more charaters').isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    // Destructuring
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = '#';
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router;
