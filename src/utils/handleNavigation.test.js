9969bff20d9504dd42a29570ed5297815d58694f
Quymbee Chen <quymbee@mattchen.com>
2021-04-29 18:38:58 -0400

    update handleNavigation test

--- src/utils/handleNavigation.test.js
+++ src/utils/handleNavigation.test.js
@@ -1,19 +1,17 @@
-
 import handleNavigation from "./handleNavigation";
 
-const examplePage = {
-    page: "person-alert",
-    name: "Rebecca Smith",
-    index: "2",
-
-};
-
 describe("handleNavigation", () => {
-    test("handle the navigation", () => {
-        const expected = "/person-alert/2/Rebecca-Smith";
-        const actual = handleNavigation("person-alert", "Rebecca Smith", "2");
+  test("correctly returns proper url params for person-alert page", () => {
+    const expected = "/person-alert/2/Rebecca-Smith";
+    const actual = handleNavigation("person-alert", "Rebecca Smith", "2");
 
-        expect(actual).toEqual(expected);
-    });
+    expect(actual).toEqual(expected);
+  });
+
+  test("correctly returns proper url params for profile page", () => {
+    const expected = "/profile/2/Rebecca-Smith-Jones";
+    const actual = handleNavigation("profile", "Rebecca Smith Jones", "2");
+
+    expect(actual).toEqual(expected);
+  });
 });
-
