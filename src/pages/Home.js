fd190ab3b6d71780d851ffc85c25e946401331d6
Raju Dahal <yugineruit2012@gmail.com>
2021-05-11 03:36:03 -0400

    homepage styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,43 @@
 import React from "react";
-import homeImg from "images/home.png";
+import homeImg from "images/homeImage.png";
 import { Button} from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
-//import { Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import "./homepage.css";
 //import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 
 console.log(homeImg);
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+    padding: "10px",
+    alignItem: "center"
+  },
+  title: {
+    fontWeight:"bold",alignItem:"center", marginBottom:"150px", position: "center", justifyContent: "center", justify: "center"
+  },
+  center: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  Grid: {
+  container: "row",
+  justify: "center",
+  }
+}));
+
+
 const Home = () => {
 
-  const useStyles = makeStyles((theme) => ({
-    button: {
-      margin: theme.spacing(1),
-    },
-  }));
   const classes = useStyles();
-
 
   return <>
     
-   
-    <div style={{float:"left"}}>
-    <div style={{marginTop:"20%",float:"left",width:"100%",height:"45%",fontSize:"100px",color:"red"}}>
-    
-      AMBER+
-    
-    </div>
-   
-    <div>
-    
-    
+   <div className={classes.center}>
+    <div class="grid-container">
+  <div class="A"> <Typography variant="h1" className={classes.title}>AMBER+</Typography>
     <Button
         variant="contained"
         color="primary"
@@ -39,11 +46,13 @@ const Home = () => {
       >
         View Alerts
         </Button>
-    </div>
-    </div>
-   
-    <img style={{color:"white",background:"wide", float: "right",width:"50%"}} src={homeImg} alt = "homeImg"/>
+  </div>
+  <div class="B">
+  <img style={{ color:"white",background:"wide", width:"80%",height: "100%"}} src={homeImg} alt = "homeImg"/>
     
+  </div>
+</div>
+    </div>
     </>;
 };
 
