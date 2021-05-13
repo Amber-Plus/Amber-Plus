ac145789a88a7c9ff6c7b9940eadfe3b67835484
Quymbee Chen <quymbee@mattchen.com>
2021-05-13 04:04:10 -0400

    clean up home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,66 @@
 import React from "react";
-import homeImg from "images/homeImage.png";
-import { Button} from "@material-ui/core";
-import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import "./homepage.css";
-//import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { Button, Typography, Grid } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import CustomContainer from 'components/common/CustomContainer'
+import homeImg from "constants/homeImage.png";
+import { PAGE_ROUTES, MISSING } from "constants/pages";
 
-
-console.log(homeImg);
 const useStyles = makeStyles((theme) => ({
   button: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(5, 1, 1, 0),
     padding: "10px",
-    alignItem: "center"
+  },
+  btnText: {
+    display: "flex",
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: theme.spacing(0, 3)
   },
   title: {
-    fontWeight:"bold",alignItem:"center", marginBottom:"150px", position: "center", justifyContent: "center", justify: "center"
+    fontSize: theme.spacing(10),
+    fontWeight: "bold",
+    width: '100%'
   },
-  center: {
-    display: "flex",
-    justifyContent: "center",
-  },
-  Grid: {
-  container: "row",
-  justify: "center",
+  img: {
+    width: "100%",
+    height: "90%"
   }
 }));
 
-
 const Home = () => {
-
   const classes = useStyles();
 
-  return <>
-    
-   <div className={classes.center}>
-    <div class="grid-container">
-  <div class="A"> <Typography variant="h1" className={classes.title}>AMBER+</Typography>
-    <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        startIcon={<SearchIcon />}
-      >
-        View Alerts
-        </Button>
-  </div>
-  <div class="B">
-  <img style={{ color:"white",background:"wide", width:"80%",height: "100%"}} src={homeImg} alt = "homeImg"/>
-    
-  </div>
-</div>
-    </div>
-    </>;
+  return <CustomContainer >
+    <Grid container justify='center' alignItems='center'>
+      <Grid container item justify='center' direction='column' xs={4} sm={4} md={4} lg={4}>
+        <Grid item xs={8}>
+          <Typography className={classes.title}>Amber+</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>The platform that helps others help each other in order to find loved ones.</Typography>
+        </Grid>
+
+        <Grid item xs={8}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<SearchIcon />}
+            component='a'
+            href={PAGE_ROUTES[MISSING]}
+          >
+            <span className={classes.btnText} >View Alerts</span>
+
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container item justify='center' alignItems='center' xs={8} sm={8} md={8} lg={8}>
+        <img className={classes.img} src={homeImg} alt="homeImg" />
+      </Grid>
+    </Grid>
+  </CustomContainer>;
 };
 
 export default Home;
